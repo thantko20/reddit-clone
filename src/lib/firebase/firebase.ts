4f8f5f1c2e796183e5c5b4989b3d2ffd56b4165c
Thant Ko Zaw <thantmkou10@gmail.com>
2022-06-26 21:25:36 +0630

    Add auth

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,4 +1,7 @@
 import { firebaseConfig } from './config';
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
