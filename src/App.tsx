583a8643f41adfdd09766a7f529146f7d490d1fd
Thant Ko Zaw <thantmkou10@gmail.com>
2022-06-26 21:44:42 +0630

    Add react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
     <Router>
+      <Toaster />
       <h1>Hello World</h1>
     </Router>
   );
