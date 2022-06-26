3239040c97b4ccd0eeb2d28402852715bc47abd2
Thant Ko Zaw <thantmkou10@gmail.com>
2022-06-26 19:46:27 +0630

    Typescript Integration

---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBOPR8BSPLcQQ585iS5naB2KR53wVNQZKs',
+  authDomain: 'reddit-clone-marco1.firebaseapp.com',
+  projectId: 'reddit-clone-marco1',
+  storageBucket: 'reddit-clone-marco1.appspot.com',
+  messagingSenderId: '343200661202',
+  appId: '1:343200661202:web:f413c15971cc1d0c61b70b',
+};
---
+++ src/lib/firebase/firebase.ts
@@ -0,0 +1,4 @@
+import { firebaseConfig } from './config';
+import { initializeApp } from 'firebase/app';
+
+const app = initializeApp(firebaseConfig);
