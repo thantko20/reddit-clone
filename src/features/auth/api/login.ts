e253491d2c62bc67bda79eb32b57a6e3e770d2df
Thant Ko Zaw <thantmkou10@gmail.com>
2022-06-27 10:21:06 +0630

    Encapsulate the fn in hook

--- src/features/auth/api/login.ts
+++ src/features/auth/api/login.ts
@@ -1,19 +1,32 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '@/lib/firebase/firebase';
 import toast from 'react-hot-toast';
+import { useState } from 'react';
 
 export type LoginCredentials = {
   email: string;
   password: string;
 };
 
-export const loginWithEmailAndPassword = async ({
-  email,
-  password,
-}: LoginCredentials) => {
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-  } catch {
-    toast.error('There was a problem logging in');
-  }
+export const useLoginWithEmailAndPassword = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loginWithEmailAndPassword = async ({
+    email,
+    password,
+  }: LoginCredentials) => {
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      setIsLoading(false);
+    } catch {
+      toast.error('There was a problem logging in');
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    loginWithEmailAndPassword,
+    isLoading,
+  };
 };
