eff40098227502054c00e8e27706c40bb721f5df
Thant Ko Zaw <thantmkou10@gmail.com>
2022-07-01 12:43:16 +0630

    Get user from db after logging in

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,13 +1,17 @@
-import React, { createContext, useContext } from 'react';
-import { auth } from '@/lib/firebase/firebase';
-import { Auth } from 'firebase/auth';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth, db } from 'lib/firebase/firebase';
+import { Auth, onAuthStateChanged } from 'firebase/auth';
+import { UserType } from 'features/auth/types';
+import { doc, getDoc } from 'firebase/firestore';
 
 export interface AuthContextValue {
   auth: Auth | null;
+  user: UserType;
 }
 
 const initialAuthValue: AuthContextValue = {
   auth: null,
+  user: null,
 };
 
 const AuthContext = createContext<AuthContextValue>(initialAuthValue);
@@ -20,7 +24,29 @@ export interface AuthProviderProps {
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const value = { auth };
+  const [user, setUser] = useState<UserType>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async () => {
+      await getUser();
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const getUser = async () => {
+    if (!auth.currentUser) {
+      setUser(null);
+      return;
+    }
+
+    const userRef = doc(db, 'users', auth.currentUser.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (userSnap.exists()) setUser(userSnap.data() as UserType);
+  };
+
+  const value = { auth, user };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
