f2292f8fade8cbafa171f3d9bb5fffc7cc5d0978
Thant Ko Zaw <thantmkou10@gmail.com>
2022-07-01 13:42:01 +0630

    decrease button sizes

--- src/components/Elements/Button/Button.tsx
+++ src/components/Elements/Button/Button.tsx
@@ -9,8 +9,8 @@ const variants = {
 };
 
 const sizes = {
-  sm: 'px-4 py-1 text-sm',
-  md: 'px-5 py-2 text-sm',
+  sm: 'px-3 py-0.5 text-sm',
+  md: 'px-4 py-1 text-sm',
   lg: 'px-6 py-3 text-base',
 };
 
