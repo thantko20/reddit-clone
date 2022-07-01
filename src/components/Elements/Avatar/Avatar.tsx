61827d1e60beff9987f3821e4568808532a8b757
Thant Ko Zaw <thantmkou10@gmail.com>
2022-07-01 13:41:36 +0630

    Add avatar

---
+++ src/components/Elements/Avatar/Avatar.tsx
@@ -0,0 +1,33 @@
+import clsx from 'clsx';
+
+const sizes = {
+  sm: 'w-6 h-6',
+  md: 'w-8 h-8',
+  lg: 'w-10 h-10',
+};
+
+export interface AvatarProps {
+  url: string;
+  size?: keyof typeof sizes;
+  className?: string;
+  alt?: string;
+}
+
+export const Avatar = ({
+  url,
+  size = 'md',
+  className = '',
+  alt = 'avatar',
+}: AvatarProps) => {
+  return (
+    <img
+      className={clsx(
+        'bg-white rounded-full w-8 h-8 overflow-hidden',
+        size,
+        className,
+      )}
+      src={url}
+      alt={alt}
+    />
+  );
+};
